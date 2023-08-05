9e68a464800339d87c7645b2e80c860792ab56b1
ats1104 <yoshinaga@st.cs.kumamoto-u.ac.jp>
2023-08-05 13:18:20 +0900

    日付の入力フォーマットを変更

--- oneday_plan_page.py
+++ oneday_plan_page.py
@@ -10,16 +10,22 @@ class OneDayPlanPage(Page):
     def __init__(self, page_name, next_pages, main_window_flag):
         super().__init__(page_name, next_pages, main_window_flag)
         header = ['ユーザー名', 'タスク', '予定日', '期限', '詳細', '難易度']
+        years = [str(i) for i in range(2022, 2031)]
+        months = [str(i) for i in range(1, 13)]
+        days = [str(i) for i in range(1, 32)]
         self.schedule = []
         self.db_system = MySchedule()
         self.this_layout = [
             [sg.Text(self.page_name)],
-            [sg.Text('日付入力'), sg.Input(key='-DATE-'), sg.Button('送信', key = self.submit_button_func), sg.Button('終了', key = self.go_back)], 
+             [sg.Text('日付指定'), sg.Combo(years, size=(10, 5), key='-YEAR-'), sg.Text('年'), sg.Combo(months, size=(10, 5), key='-MONTH-'), sg.Text('月'), sg.Combo(days, size=(10, 5), key='-DATE-'), sg.Text('日')],
+            [sg.Button('送信', key = self.submit_button_func), sg.Button('終了', key = self.go_back)], 
             [sg.Table(self.schedule, headings=header, key='table')]
         ]
     
     def submit_button_func(self):
-        do_date = self.my_values['-DATE-']
+        
+        do_date = date(int(self.my_values['-YEAR-']), int(self.my_values['-MONTH-']), int(self.my_values['-DATE-']))
+        print(do_date)
         df = self.db_system.get_shcs('Alice', do_date=f"'{do_date}'")
         schedule = df.values.tolist()
         self.window['table'].update(values=schedule)
