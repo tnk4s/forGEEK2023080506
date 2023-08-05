f092fbb88ed863173dc6bc857835045a0307669d
ats1104 <yoshinaga@st.cs.kumamoto-u.ac.jp>
2023-08-05 14:19:32 +0900

    レイアウトと，サブミットボタンを改良しました

--- oneday_plan_page.py
+++ oneday_plan_page.py
@@ -10,17 +10,27 @@ class OneDayPlanPage(Page):
     def __init__(self, page_name, next_pages, main_window_flag):
         super().__init__(page_name, next_pages, main_window_flag)
         header = ['ユーザー名', 'タスク', '予定日', '期限', '詳細', '難易度']
+        width = [10, 10, 10, 10, 10, 10]
         years = [str(i) for i in range(2022, 2031)]
         months = [str(i) for i in range(1, 13)]
         days = [str(i) for i in range(1, 32)]
         self.schedule = []
         self.db_system = MySchedule()
-        self.this_layout = [
-            [sg.Text(self.page_name)],
+        frame1 = sg.Frame('入力フォーム', [[sg.Text(self.page_name)],
              [sg.Text('日付指定'), sg.Combo(years, size=(10, 5), key='-YEAR-'), sg.Text('年'), sg.Combo(months, size=(10, 5), key='-MONTH-'), sg.Text('月'), sg.Combo(days, size=(10, 5), key='-DATE-'), sg.Text('日')],
-            [sg.Button('送信', key = self.submit_button_func), sg.Button('終了', key = self.go_back)], 
-            [sg.Table(self.schedule, headings=header, key='table')]
-        ]
+            [sg.Button('送信', key = self.submit_button_func), sg.Button('終了', key = self.go_back)]],size=(500, 700))
+
+        frame2 = sg.Frame('計画表', [[sg.Table(self.schedule, headings=header, col_widths=width, key='-TABLE-', size=(400, 500))]], size=(400, 700))
+        self.this_layout = [
+            [frame1,
+             frame2]
+             ]
+        # self.this_layout = [
+        #     [sg.Text(self.page_name)],
+        #      [sg.Text('日付指定'), sg.Combo(years, size=(10, 5), key='-YEAR-'), sg.Text('年'), sg.Combo(months, size=(10, 5), key='-MONTH-'), sg.Text('月'), sg.Combo(days, size=(10, 5), key='-DATE-'), sg.Text('日')],
+        #     [sg.Button('送信', key = self.submit_button_func), sg.Button('終了', key = self.go_back)], 
+        #     [sg.Table(self.schedule, headings=header, key='-TABLE-', size=(160, 80))]
+        # ]
     
     def submit_button_func(self):
         
@@ -28,7 +38,7 @@ class OneDayPlanPage(Page):
         print(do_date)
         df = self.db_system.get_shcs('Alice', do_date=f"'{do_date}'")
         schedule = df.values.tolist()
-        self.window['table'].update(values=schedule)
+        self.window['-TABLE-'].update(values=schedule)
 
         return self.stable_flag
     
